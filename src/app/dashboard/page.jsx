d0a261d50223013e2337f56b5cf6528c52c8022a
RUSHi-VAiRALE <rushivairale932@gmail.com>
2024-12-03 17:01:25 +0530

    done with blog app

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,20 @@
+'use client'
+
 import React from 'react'
+import useSWR from 'swr';
 
 export default function Dashboard() {
+  const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+
+  
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+  console.log(data);
+
   return (
     <div>Dashboard</div>
   )
